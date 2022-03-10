cb55b99ba0f39c9986478f32b09320eb71b91bbb
samuelflopes <samuelsamuelferreira.199807@gmail.com>
2022-03-10 14:57:16 -0300

    Add arquivo filebase de banco de dados

---
+++ src/database/filebase.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDoGf7TqcGc7Axa6fUFoFhkjY22BSSNCYU",
+    authDomain: "app-crud-e7ae5.firebaseapp.com",
+    projectId: "app-crud-e7ae5",
+    storageBucket: "app-crud-e7ae5.appspot.com",
+    messagingSenderId: "280585050835",
+    appId: "1:280585050835:web:09f8aa8f787922fc9dd226"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
