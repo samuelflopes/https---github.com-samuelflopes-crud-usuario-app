167720b82e5eba2be46798793e61aa0b267e54d5
samuelflopes <samuelsamuelferreira.199807@gmail.com>
2022-03-11 12:51:01 -0300

    Ajuste na aparencia inicial da tela e add campos do form

--- src/component/FormsRegister.js
+++ src/component/FormsRegister.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FormsRegister = () =>  {
+
+    //Variable capture dados
+const initialvalues = {
+    nome: '',
+    telefone:'',
+    cpf: '',
+    cep: '',
+    Logradouro: '',
+    cidadeEstado: ''
+}
+
+//captura e manipulação dos valores 
+let { valor, setValues } = useState( initialvalues )
+
     return (
-        <h1>Formulario</h1>
+        <h1> Formulario </h1>
     )
 
 }
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import FormsRegister from './FormsRegister';
 
 const Register = () => {
     return (
-        <h1>Cadastro</h1>
+        <div>
+            <div className = "jumbotron jumbotron-fluid">
+                <div className = "container">
+                    <h1 className = "display-4">Cadastro de usuários</h1>
+                    <p className = "lead">This is horizontal space of its parent.</p>
+                </div>
+            </div>
+                <div className = "row">
+                    <div className = "col-md-5">
+                        <FormsRegister />
+                    </div>
+                    <div>
+                        <h2>Lista de pessoas cadastradas</h2>
+                    </div>    
+            </div>
+                
+  
+        </div>
     )
 }
 
